5287388668a2c69a21a73a527f835a108c7d852c
Emrullah Taştan <emrullah69@gmail.com>
2021-10-21 17:41:31 +0300

    change new list item design

--- src/components/leftSideBar/leftSideBar.js
+++ src/components/leftSideBar/leftSideBar.js
@@ -19,49 +19,46 @@ export class LeftSideBar extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className={"left-side-bar"}>
-                    <div className={"left-side-bar__header"}>
-                        <button className={"left-side-bar__header__button"}>
-                            <FontAwesomeIcon className={"left-side-bar__header__button__icon"} icon={faChevronLeft}/>
+            <div className={"left-side-bar"}>
+                <div className={"left-side-bar__top"}>
+                    <div className={"left-side-bar__top__header"}>
+                        <button className={"left-side-bar__top__header__button"}>
+                            <FontAwesomeIcon className={"left-side-bar__top__header__button__icon"} icon={faChevronLeft}/>
                         </button>
                     </div>
-                    <div className={"left-side-bar__area"}>
-                        {this.state.menuList.map(function (object, i) {
-                            return <ListItem key={i} title={object.title} icon={object.icon} number={object.number}/>;
-                        })}
-                        <ListItem/>
+                    <div className={"left-side-bar__top__middle"}>
+                        <div className={"left-side-bar__middle__area"}>
+                            {this.state.menuList.map(function (object, i) {
+                                return <ListItem key={i} title={object.title} icon={object.icon} number={object.number}/>;
+                            })}
+                            <ListItem/>
+                        </div>
+                        <div className={"left-side-bar__area"}>
+                            {this.state.userMenuList.map(function (object, i) {
+                                return <ListItem key={i} title={object.title} icon={object.icon} number={object.number}/>;
+                            })}
+                        </div>
                     </div>
-                    <div className={"left-side-bar__area"}>
-                        {this.state.userMenuList.map(function (object, i) {
-                            return <ListItem key={i} title={object.title} icon={object.icon} number={object.number}/>;
-                        })}
+                    <div className={"left-side-bar__area left-side-bar-new-list-area"}>
+                        <div className={"left-side-bar-new-list-area__left"}>
+                            <div className={"left-side-bar-new-list-area__left__icon"}>
+                                <FontAwesomeIcon icon={faPlus}/>
+                            </div>
+                            <div className={"left-side-bar-new-list-area__left__input-area"}>
+                                <input className={"left-side-bar-new-list-area__left__input-area__input"} placeholder={"New list"} onKeyDown={this.enterValueToNewList}/>
+                            </div>
+                        </div>
+                        <div className={"left-side-bar-new-list-area__right"}>
+                            <FontAwesomeIcon icon={faFolderPlus}/>
+                        </div>
                     </div>
                 </div>
-
                 <div className={"left-side-bar__bottom"}>
-                    <div>
-                        <div className={"left-side-bar__area left-side-bar-new-list-area"}>
-                            <div className={"left-side-bar-new-list-area__left"}>
-                                <div className={"left-side-bar-new-list-area__left__icon"}>
-                                    <FontAwesomeIcon icon={faPlus}/>
-                                </div>
-                                <div className={"left-side-bar-new-list-area__left__input-area"}>
-                                    <input className={"left-side-bar-new-list-area__left__input-area__input"} placeholder={"New list"} onKeyDown={this.enterValueToNewList}/>
-                                </div>
-                            </div>
-                            <div className={"left-side-bar-new-list-area__right"}>
-                                <FontAwesomeIcon icon={faFolderPlus}/>
-                            </div>
-                        </div>
-                        <div className={"left-side-bar__bottom-buttons"} style={{backgroundColor: "lightgreen"}}>
-                            x
-                        </div>
+                    <div className={"left-side-bar__bottom-buttons"} style={{backgroundColor: "lightgreen"}}>
+                        x
                     </div>
                 </div>
-
             </div>
-
         )
     }
 }
